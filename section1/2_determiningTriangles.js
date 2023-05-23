5546be4f6fd4ffb0f0f3184cf196e6bb2225249b
leejisoo <leejisoo@atnsgroup.com>
2023-05-23 15:48:31 +0900

    삼각형 판별하기

---
+++ section1/2_determiningTriangles.js
@@ -0,0 +1,21 @@
+function solution(a, b, c){
+    /**
+    let answer;
+    if(a > b) answer = a;
+    else answer = b
+    if(c > answer) answer = c;
+    if((a + b + c - answer) > answer) answer = "YES";
+    else answer = "NO";
+    return answer;
+     **/
+    let answer = "YES", max;
+    let sum = a + b + c;
+    if(a > b) max = a;
+    else max = b;
+    if(c>max) max = c;
+    if((sum - max) <= max) answer = "NO"
+    return answer;
+}
+
+console.log(solution(13,33,17));
+//완
