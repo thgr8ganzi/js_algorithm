fa9b64b6ad3bbf4dba7b92d1b118ed7d473132e8
leejisoo <leejisoo@atnsgroup.com>
2023-06-13 17:04:23 +0900

    최솟값 구하기

---
+++ section1/4_sumFrom1ToN.js
@@ -0,0 +1,10 @@
+function solution(n){
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum
+}
+
+console.log(solution(10))
+//완
---
+++ section1/5_findTheMinimum.js
@@ -0,0 +1,24 @@
+function solution(arr){
+    /*
+    let answer;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length - 1; j++) {
+            if(arr[i] > arr[j]){
+                arr[i] = answer;
+                arr[i] = arr[j];
+                arr[j] = answer
+            }
+        }
+    }
+    return arr[0];
+    */
+    let answer, min=Number.MAX_SAFE_INTEGER;
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i]<min) min=arr[i]
+    }
+    answer=min;
+    return answer
+}
+let arr=[5, 7, 1, 3, 2, 9, 11];
+console.log(solution(arr));
+//완
