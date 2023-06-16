b43a49e8ef517e1ca9d04a4809b9805abe64d27b
leejisoo <leejisoo@atnsgroup.com>
2023-06-16 13:16:58 +0900

    함수형 테스트

--- section1/functionTest.js
+++ section1/functionTest.js
@@ -17,20 +17,40 @@ let array = [
     },
 ]
 
-function arrayCopy(arr){
-    let copy = arr.slice();
-    for (let i = 0; i < copy.length; i++) {
-        if(copy[i].name === 'd'){
-            copy[i] = ObjCopy(copy[i]);
-        }
+// function arrayCopy(arr){
+//     let copy = arr.slice();
+//     for (let i = 0; i < copy.length; i++) {
+//         if(copy[i].name === 'd'){
+//             copy[i] = ObjCopy(copy[i]);
+//         }
+//     }
+//     return copy
+// }
+// function ObjCopy(copy_arr){
+//     let copy_obj = Object.assign({},copy_arr);
+//     copy_obj.name = 'abc'
+//     return copy_obj
+// }
+// let ar = arrayCopy(array)
+// console.log(ar)
+// console.log(array)
+
+function removeItems(array, idx, count){
+    let copy = array.slice();
+    copy.splice(idx, count)
+    return copy;
+}
+function remove_item_by_name(cart,name){
+    let idx = null;
+    for (let i = 0; i < cart.length; i++) {
+        if(cart[i].name === name) idx = i;
     }
-    return copy
+    if(idx !== null)
+        return removeItems(cart, idx, 1);
+    return cart;
 }
-function ObjCopy(copy_arr){
-    let copy_obj = Object.assign({},copy_arr);
-    copy_obj.name = 'abc'
-    return copy_obj
-}
-let ar = arrayCopy(array)
-console.log(ar)
-console.log(array)
+
+console.log(
+    remove_item_by_name(array, 'c'),
+    array
+)
