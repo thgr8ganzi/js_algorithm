13ea2cef9236f34702f72757ff35707f31d76e70
leejisoo <leejisoo@atnsgroup.com>
2023-06-26 17:09:41 +0900

    일곱난장이

--- section1/8_sevenDwarfs.js
+++ section1/8_sevenDwarfs.js
@@ -2,35 +2,42 @@ function solution(arr){
     /*
     let answer = 0;
     let i,j = 0;
+    let flag = false;
     for (let k = 0; k < arr.length; k++) {
         answer += arr[k]
     }
     for (let k = 0; k < arr.length; k++) {
-        for (let l = k + 1; l < arr.length; l++) {
+        for (let l = k + 1; l < arr.length - 1; l++) {
             if(answer - (arr[k] + arr[l]) === 100){
                 i = k;
                 j = l;
-                break
+                flag = true
+                break;
             }
         }
+            if(flag) break;
     }
     let result = arr.filter((num, index) => index !== i && index !== j);
     return result;
      */
     let answer = arr;
+    let flag = 0;
     let sum = arr.reduce((a, b) => a + b, 0)
     for (let i = 0; i < 8; i++) {
         for (let j = i + 1; j < 9; j++) {
             if((sum - (arr[i] + arr[j])) === 100){
                 arr.splice(j,1)
                 arr.splice(i,1)
+                flag = 1;
+                break;
             }
         }
+            if(flag === 1) break;
     }
 
     return answer;
 }
 
-let arr=[20, 7, 23, 19, 10, 15, 25, 8, 13];
+let arr=[23, 7, 20, 19, 10, 15, 25, 8, 13];
 console.log(solution(arr));
 // 완
