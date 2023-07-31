da10332c047cf690800ee7cc29caef9b5881b2e3
leejisoo <leejisoo@atnsgroup.com>
2023-07-31 16:45:35 +0900

    문자열 압축

---
+++ section3/5_string compression.js
@@ -0,0 +1,16 @@
+function solution(s){
+    let answer="";
+    let cnt = 1;
+    for (let i = 0; i < s.length; i++) {
+        if(s[i] === s[i + 1]){
+            cnt ++;
+        } else{
+            answer += s[i] + (cnt === 1 ? '' : cnt);
+            cnt = 1;
+        }
+    }
+    return answer;
+}
+
+let str="KKHSSSSSSSE";
+console.log(solution(str));
